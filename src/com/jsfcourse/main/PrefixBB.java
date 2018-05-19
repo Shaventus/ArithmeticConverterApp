78ad2faa256a8c48cdcb48b5d3278806056a992c
Anna Grzywnowicz <ann.grzywnowicz@gmail.com>
2018-05-19 22:48:09 +0200

    Added test project

---
+++ src/com/jsfcourse/main/PrefixBB.java
@@ -0,0 +1,17 @@
+package com.jsfcourse.main;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+@ViewScoped
+@ManagedBean
+public class PrefixBB implements Serializable
+{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
+	
